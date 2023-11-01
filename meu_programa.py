a0c84cc8beb804c6868800874c24de277ca91817
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2023-11-01 18:16:58 +0900

    feed code

--- meu_programa.py
+++ meu_programa.py
@@ -2,3 +2,21 @@ valor_fixo = 45
 resposta =int( input("Informe um numero de 1 a 10: "))
 soma = valor_fixo + resposta
 print(resposta)
+
+# Crie um programa que solicite o nome completo do usuário e imprima "Obrigado por responder à pergunta fulano", onde "fulano" é o nome informado pelo usuário.
+
+nome_completo = input("Informe o seu nome completo: ")
+print(f"Obrigado por responder à pergunta {nome_completo}")
+
+# Create a program that asks the user which drink they prefer with the options 1) Water, 2) Beer, 3) Wine and 4) Milk. This way:
+
+# Requests the user's favorite drink
+
+print("Which drink do you prefer?")
+print("1)Water")
+print("2)Beer")
+print("3)Wine")
+print("4)Milk")
+option = input("Inform your number: ")
+
+print(f"You choose the option {option}.")
