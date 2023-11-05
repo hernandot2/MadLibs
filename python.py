aa3034eb69211ef98a05e2f299b196335a4c6114
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2023-11-05 13:40:55 +0900

    feed code

---
+++ python.py
@@ -0,0 +1,17 @@
+
+
+# Interpolacao
+valor_unitario = 165.78
+quantidade = 5 
+valor_total = valor_unitario * quantidade 
+frase = "O produto custa %f. Eu comprei %d. Paguei ao todo %f" % ( valor_unitario, quantidade, valor_total)
+print(frase)
+
+
+valor_unitario = 165.78
+quantidade = 5 
+valor_total = valor_unitario * quantidade 
+frase = f"O produto custa {valor_unitario}. Eu comprei {quantidade}. Paguei ao todo {valor_unitario * quantidade}"
+print(frase)
+
+
