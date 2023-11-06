485d28527f4a242aa7a69230353a9b3ffae235f0
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2023-11-06 21:14:00 +0900

    feed code

--- python.py
+++ python.py
@@ -45,7 +45,7 @@ quantidade = int(input('Informe a quantidade de produtos: '))
 total = preco * quantidade
 
 print('O produto %s custa R$ %.2f, você comprou %d e vai pagar R$ %.2f' % (nome, preco, quantidade, total))
-O produto beer custa R$ 34.00, você comprou 2 e vai pagar R$ 68.00
+
 
 # e strings - Aula 2
 # Aprendendo mais sobre strings
@@ -53,7 +53,7 @@ O produto beer custa R$ 34.00, você comprou 2 e vai pagar R$ 68.00
 email = "evaldowolkers@gmail.com"
 indice_arroba = email.index ("@")
 usuario = email[0:indice_arroba]
-print ("O nome de usuário é:" usuario)
+print ("O nome de usuário é:", usuario)
 
 # O nome do provedor 
 email = "evaldowolkers@gmail.com"
@@ -61,3 +61,26 @@ indice_arroba = email.index ("@")
 indice_ponto = email.index(".")
 provedor = email[indice_arroba+1:indice_ponto]
 print ("O nome do provedor é:", provedor)
+
+# outro exercicio 
+# Qual é o resultado do código a seguir caso o usuário informe o e-mail maria.silva@teste.com.br?
+
+email = input("Informe seu e-mail: ")
+ 
+var1, var2 = email.split("@")
+# var1 = maria.silva , 
+# var2 = teste.com.br 
+var3, var4 = var1.split(".")
+# var3 = maria
+# var4 = silva 
+
+var5, var6, var7 = var2.split(".")
+# var5 = teste
+# var6 = com 
+# var7 = br
+
+print(f"var3: {var3}\n"
+      f"var4: {var4}\n"
+      f"var5: {var5}\n"
+      f"var6: {var6}\n"
+      f"var7: {var7}\n")
