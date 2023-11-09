2f623e3a10d9a9b5f43c70e5e2d1d264bb0a06ab
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2023-11-09 23:43:28 +0900

    feed code

--- python5.py
+++ python5.py
@@ -3,7 +3,7 @@ status = int(input("Qual seu codigo de status(1-4): "))
 qty_wallets = input("qty wallets: ") 
 
 
-if ((status == 1 and  qty_wallets > 1) or (status == 2 and  qty_wallets > 2) or (status == 3 or status == 4) and  qty_wallets > 3)):
+if ((status == 1 and  qty_wallets > 1) or (status == 2 and  qty_wallets > 2) or (status == 3 or status == 4) and  qty_wallets > 3):
   print(" voce tera que pagar uma taxa de servico por excesso de bagagem!")
 else:
   comprimento = int(input( "comprimento(cm): "))
@@ -11,13 +11,7 @@ else:
   altura = int(input("altura(cm): "))
   peso  = int(input("peso(kg): "))
 
-  if ( comprimento + largura + altura + peso )
+  if ((comprimento + largura + altura + peso )) > 158 or ( peso > 32)
+  print(" voce tera que pagar uma taxa de servico por excesso de bagagem!")
 
 
-3- qtd de malas > 4
-4- elif ou else - informar largura, altura, peso 
-5- Caso contrario print(" Tudo ok com as bagagens, tenha uma boa viagem")
-
-
-
-input
