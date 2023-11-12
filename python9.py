74b8a6bc3b1e0788e6d3e9411fc93fedeea34f84
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2023-11-12 16:52:46 +0900

    feed code

---
+++ python9.py
@@ -0,0 +1,24 @@
+
+# Fazer um programinha de soma, que as pessoas informem os numeros inteiros, e
+# que possam informar a quantidade de numeros para soma. 
+# Para isso, tem que ter variaveis de somar, qtd de numero e while para ter continuidade
+# da operacao. e uma condicao que de END quando a pessoa coloca o numero negativo como quantidade
+
+soma = 0 # soma os numeros
+x = 0 # executa a operacao de somar
+qtd_numeros = int(input(" Informe a qtd de numeros p/ somar:"))
+
+while x < qtd_numeros:
+  x = x + 1
+  soma = soma + int(input(" Informe um num inteiro: "))
+
+else:
+   print(f"Soma:{soma}")
+   print("acabou")
+
+
+while True:
+    x = input("Informe o valor de x: ")
+    z = x + x
+ 
+print(z)
