67055ef098ec2acd0ac5749473d47cf4e15be943
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2023-11-12 22:58:16 +0900

    feed code

---
+++ python10.py
@@ -0,0 +1,8 @@
+
+while True:
+  resposta = input(" Voce gosta de Python? ")
+  if resposta.upper() == "SIM":
+    print (" A resposta esta correta")
+    # break
+  else:
+    print ("Opa, esta nao eh a resposta correta, tente novamente")
