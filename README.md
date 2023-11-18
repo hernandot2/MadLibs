4c620a11ae3be0c500f674aa706f0e4c6cf624fb
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2023-11-18 22:55:19 +0900

    enumerator_items

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MadLibs
+
+Separate odd and even sorting them 
 Madlibs / Rock Paper Scissors / Binary Search in Python
---
+++ python15.py
@@ -0,0 +1,9 @@
+
+seasons = ["Spring", "Summer", "Autumn", "Winter"]
+seasons_list = list(enumerate(seasons))
+print(seasons_list)
+
+seasons_list = list(enumerate(seasons, start=5))
+print(seasons_list)
+
+
