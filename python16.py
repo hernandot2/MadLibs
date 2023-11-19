4d16b0aaa02af14ba23435774ea84e9b5d01286a
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2023-11-19 20:12:49 +0900

    cloning of items

--- python16.py
+++ python16.py
@@ -10,11 +10,18 @@ itens.sort() # Deixar ordenado
 for indice, items in enumerate(itens):
   print(indice+1, items)
 
+lista_numeros = [1,2,3,4,5]
+nova_lista = lista_numeros 
 
+print(lista_numeros) # Resultado: [1,2,3,4,5]
+print(nova_lista)    # Resultado: [1,2,3,4,5]
 
+lista_numeros += [6]
+print(lista_numeros) # Resultado: [1,2,3,4,5,6]
+print(nova_lista)    # Resultado: [1,2,3,4,5,6]
 
-
-
-
+nova_lista += [7]
+print(lista_numeros) # Resultado: [1,2,3,4,5,6,7]
+print(nova_lista)    # Resultado: [1,2,3,4,5,6,7]
 
 
