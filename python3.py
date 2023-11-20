738f263d8a37fa408babee40887ed508651f289d
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2023-11-20 13:55:46 +0900

    Data structure extraction

--- python3.py
+++ python3.py
@@ -1,2 +1,11 @@
 
+# Slice
+linguagens = [ "Python", "Cobol", "Clipper", "C", "C++", "Go", "JavaScript"]
+linguagens2 = linguagens[3:5] # 3,4 o 5 nao eh incluido pela regra geral
+print(linguagens)
+print(linguagens2)
+
+# Resultado
+["Python", "Cobol", "Clipper", "C", "C++", "Go", "JavaScript"]
+["C", "C++"]
 
