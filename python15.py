5fdeac8bc7ca912bcba641782db996901407f386
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2023-12-15 23:54:29 +0900

    seasons code added

--- python15.py
+++ python15.py
@@ -7,3 +7,11 @@ seasons_list = list(enumerate(seasons, start=5))
 print(seasons_list)
 
 
+seasons = ["Spring", "Summer", "Autumn", "Winter"]
+seasons_list = list(enumerate(seasons))
+print(seasons_list)
+
+seasons_list = list(enumerate(seasons, start=5))
+print(seasons_list)
+
+
