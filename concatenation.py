68ad8beb18d7550b5fb41f6972680c630f26c634
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2023-12-19 23:22:41 +0900

    concatenation float

--- concatenation.py
+++ concatenation.py
@@ -16,7 +16,14 @@ idade = 28
 filhos = 2
 
 frase = "O jogador %s tem %d anos e %d filhos " %(nome, idade, filhos)
-
 print(frase)
+
 aula = 9
 print(" esta eh a aula %03d" % aula)
+
+nome = "joao"
+idade = 28
+filhos = 5
+frase = " o jogador {} tem {} anos e {} filhos." .format( nome, idade, filhos)
+
+print(frase) 
