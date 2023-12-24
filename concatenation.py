ea3f226ec604fda8aff3b5bd52a58dad263c4de9
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2023-12-24 22:16:02 +0900

    concatenation float

--- concatenation.py
+++ concatenation.py
@@ -10,12 +10,11 @@ multiplicacao = pao * 10
 print(multiplicacao)
 
 
-
 nome = "joao"
-idade = 28
-filhos = 2
+idade = 9
+filhos = 2 
 
-frase = "O jogador %s tem %d anos e %d filhos " %(nome, idade, filhos)
+frase = " o jogador %s tem %d anos e  tem %d filhos " %(nome, idade, filhos)
 print(frase)
 
 aula = 9
@@ -27,3 +26,7 @@ filhos = 5
 frase = " o jogador {} tem {} anos e {} filhos." .format( nome, idade, filhos)
 
 print(frase) 
+
+
+frase = " ola, eu vou viajar de {0}" .format('busao', 'bicicleta', 'carro')
+print(frase)
