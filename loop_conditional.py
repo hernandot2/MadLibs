9e43f69bd6ca901f86c5865a70d6d213e492553a
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2024-01-20 12:46:01 +0900

    loop conditional

--- loop_conditional.py
+++ loop_conditional.py
@@ -1,6 +1,16 @@
-lista = [] 
+# lista = [] 
+# while True:
+#   numero = int(input("Digite um numero inteiro(0 para sair):"))
+#   if numero == 0:
+#     break 
+#   lista.append(numero) 
+
+
+lista = [] # comeca-se com uma lista vazia 
 while True:
-  numero = int(input("Digite um numero inteiro(0 para sair):"))
-  if numero == 0:
-    break 
-  lista.append(numero) 
+  numero = int(input("Digite um numero inteiro(zero p sair):")) # ai quando adiciona um numero vai para o list.append(numero)
+
+  if numero == 0: # se o numero informado for zero 
+    break
+  lista.append(numero)
+print(numero)
